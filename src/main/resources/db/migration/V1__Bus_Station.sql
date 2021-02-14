1e19866e6f4f77ffc921095aeecea3977b0e45c2
andrewBatusha <andrew.batuiev@gmail.com>
2021-02-14 19:32:49 +0200

    possible fix

--- src/main/resources/db/migration/V1__Bus_Station.sql
+++ src/main/resources/db/migration/V1__Bus_Station.sql
@@ -1,6 +1,6 @@
 create table driver_licenses
 (
-    id             bigint not null
+    id serial not null
         constraint driver_licenses_pkey
             primary key,
     licence_id     varchar(255),
@@ -12,7 +12,7 @@ alter table driver_licenses
 
 create table passports
 (
-    id              bigint  not null
+    id serial not null
         constraint passports_pkey
             primary key,
     age             integer not null,
@@ -29,13 +29,13 @@ alter table passports
 
 create table drivers
 (
-    id                 bigint not null
+    id                 serial not null
         constraint drivers_pkey
             primary key,
-    driver_licenses_id bigint
+    driver_licenses_id serial
         constraint fkl1hitjn7s55w9wrekuvfxkn83
             references driver_licenses,
-    passport_id        bigint
+    passport_id        serial
         constraint fk10ls1s47nje2ul0i0miethn51
             references passports
 );
@@ -45,10 +45,10 @@ alter table drivers
 
 create table passengers
 (
-    id          bigint not null
+    id          serial not null
         constraint passengers_pkey
             primary key,
-    passport_id bigint
+    passport_id serial
         constraint fk6l6w6g5w3aev4guv8b9d94ckh
             references passports
 );
@@ -58,13 +58,13 @@ alter table passengers
 
 create table luggage
 (
-    id           bigint  not null
+    id           serial not null
         constraint luggage_pkey
             primary key,
     luggage_type varchar(255),
     name         varchar(255),
     weight       integer not null,
-    passenger_id bigint
+    passenger_id serial
         constraint fkevi0rh6432rbs52qac92tqmjm
             references passengers
 );
@@ -74,7 +74,7 @@ alter table luggage
 
 create table stations
 (
-    id         bigint not null
+    id serial not null
         constraint stations_pkey
             primary key,
     head_chair varchar(255),
@@ -86,11 +86,11 @@ alter table stations
 
 create table engineers
 (
-    id         bigint not null
+    id         serial not null
         constraint engineers_pkey
             primary key,
     name       varchar(255),
-    station_id bigint
+    station_id serial
         constraint fkt8lk9x6wm6o44v7sarid4h5s1
             references stations
 );
@@ -100,16 +100,16 @@ alter table engineers
 
 create table buses
 (
-    id          bigint  not null
+    id          serial not null
         constraint buses_pkey
             primary key,
     capacity    integer not null,
     name        varchar(255),
     status      varchar(255),
-    driver_id   bigint
+    driver_id   serial
         constraint fkehawq87sfla3c9m3anighxeaf
             references drivers,
-    engineer_id bigint
+    engineer_id serial
         constraint fkdldq14pdjryhwxoas9q18t5j4
             references engineers
 );
@@ -119,14 +119,14 @@ alter table buses
 
 create table routes
 (
-    id               bigint not null
+    id     serial not null
         constraint routes_pkey
             primary key,
     departure        varchar(255),
     departure_date   varchar(255),
     destination      varchar(255),
     destination_date varchar(255),
-    bus_id           bigint
+    bus_id serial
         constraint fkfifby6x0g0k9rmd26mdmef3vt
             references buses
 );
@@ -136,14 +136,14 @@ alter table routes
 
 create table tickets
 (
-    id           bigint  not null
+    id           serial not null
         constraint tickets_pkey
             primary key,
     price        integer not null,
-    passenger_id bigint
+    passenger_id serial
         constraint fk1ds262xq345nkvs5o9ptnftwr
             references passengers,
-    route_id     bigint
+    route_id     serial
         constraint fkjndmi4mev27gnbw3m61y1hb8q
             references routes
 );
